e6a42a9c9e435e50f846d70454d276b11caaadf4
heinztamm <karl_tamm@hotmail.com>
2023-02-01 01:36:17 +0200

    finished auth flow

--- config/dev.js
+++ config/dev.js
@@ -2,5 +2,5 @@ module.exports = {
     googleClientID: '843002585189-tdmov2hbjntuiegcitj5re84e96r2e50.apps.googleusercontent.com',
     googleClientSecret: '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',
     mongoURI: 'mongodb+srv://heinrichtamm:Qk0sjigIDHF6e5Om@cluster0.xfeqe9n.mongodb.net/emailydb?retryWrites=true&w=majority',
-    cookieKey: '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'
+    cookieKey: 'whambamshambalamalallallaa'
     };
